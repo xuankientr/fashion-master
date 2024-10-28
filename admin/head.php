5d20c2763f54e0ee546623d85ed0fdc336c07e93
kien trinh <trinhxuankien.wk@gmail.com>
2024-10-28 10:09:39 +0700

    first commit

---
+++ admin/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta content="initial-scale=1, minimum-scale=1, width=device-width" name="viewport">
+  <meta name="robots" content="all,follow">
+  <title>Admin Adam
+  </title>
+  <link rel="icon" href="../assets\images\logo\logo-adam-store-removebg-preview.png" sizes="32x32">
+  <!-- inject:css -->
+  <link rel="stylesheet" href="vendors/fomantic-ui/semantic.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&display=swap">
+  <!-- endinject -->
+  <!-- chartjs:css -->
+  <link rel="stylesheet" href="vendors/chart.js/Chart.min.css">
+  <!-- endinject -->
+</head>
